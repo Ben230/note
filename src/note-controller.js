3e43b00ab1af5cdf5366918a454d3a4b2c64723f
Ben Stuart <benarthurstuart@hotmail.com>
2019-11-28 15:22:28 +0000

    note controller spike complete

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,3 +1,20 @@
-var element = document.getElementById("app")
+// var element = document.getElementById("app")
+// element.innerHTML = "howdy"
 
-element.innerHTML = "howdy"
+
+
+(function(exports) {
+  function NoteController(noteBook) {
+    this.noteBook = noteBook;
+    var note1 = new Note("Favourite drink: seltzer");
+    this.noteBook.addNote(note1);
+    this.noteView = new NoteListView(this.noteBook);
+  };
+
+  NoteController.prototype.insertHTML = function() {
+    var element = document.getElementById("app");
+    element.innerHTML = this.noteView.return();
+  };
+
+  exports.NoteController = NoteController;
+})(this);
